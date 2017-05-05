867dc1533d036bc6f7d261f26e04d8379cd057de
eric61011 <eric61011@gmail.com>
2017-05-05 03:39:55 -0400

    finish edit book feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+  before_action :find_book, only: [:edit, :update]
   
   def index
     @books = Book.all
@@ -19,9 +19,26 @@ class BooksController < ApplicationController
     end
   end
   
+  def edit
+
+  end
+  
+  def update 
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render 'edit'
+    end    
+  end
+  
   
   private
   def book_params
     params.require("book").permit(:title, :description, :author, :price)
+  end
+  
+  def find_book
+    @book = Book.find_by(id: params[:id])
+    redirect_to books_path, notice: "no data!" if @book.nil?
   end
 end
